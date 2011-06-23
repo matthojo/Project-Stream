7d1940f2c8107d08b53772c8db193dc637ca716a
Matthew Harrison-Jones <matthojo@gmail.com>
2011-06-23 17:48:50 +0100

    Added some animation to the homepage

--- Front/js/script.js
+++ Front/js/script.js
@@ -2,7 +2,51 @@
 
 */
 
+$(document).ready(function() {
 
+	$(".clock").hover(
+	function(){
+		$(".clock span").fadeIn();
+		},function(){
+			 $(".clock span").fadeOut(100);
+		}
+	);
+	
+	$(".management").hover(
+	function(){
+		$(".management span").fadeIn();
+		},function(){
+			 $(".management span").fadeOut(100);
+		}
+	);
+	
+	setTimeout(function(){ 
+	$('.move.clock').animate({top: '+10px'}, 400)
+	$('.move.clock').animate({top: '0px'}, 200);
+	}, 100);
+	
+	setTimeout(function(){ 
+	$('.move.plus').animate({top: '+10px'}, 400)
+	$('.move.plus').animate({top: '0px'}, 200);
+	}, 500);
+	
+	setTimeout(function(){ 
+	$('.move.management').animate({top: '+10px'}, 400)
+	$('.move.management').animate({top: '0px'}, 200);
+	}, 1000);
+	
+	setTimeout(function(){ 
+	$('.move.equals').animate({top: '+10px'}, 400)
+	$('.move.equals').animate({top: '0px'}, 200);
+	}, 1600);
+	
+	setTimeout(function(){ 
+	$('img.move').animate({top: '+10px'}, 400)
+	$('img.move').animate({top: '0px'}, 200);
+	}, 2200);
+		
+		
+});
 
 
 
